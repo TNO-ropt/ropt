29721252656e86519956fd2e983ff89a4a1f6e2f
Peter Verveer <pieter.verveer@tno.nl>
2026-01-15 12:59:52 +0100

    Update the workflow example

--- examples/rosenbrock_workflow.py
+++ examples/rosenbrock_workflow.py
@@ -1,14 +1,16 @@
 """Example of optimization of a multi-dimensional Rosenbrock test function.
 
-This example demonstrates optimization of the unmodified (deterministic)
-multi-dimensional Rosenbrock function. It shows how to write a minimal
-configuration and how to run and monitor the optimization using basic workflow
-components.
+This example demonstrates optimization of the a modified multi-dimensional
+Rosenbrock function that exhibits uncertainty in its parameters. It shows how to
+write a minimal configuration and how to run and monitor the optimization using
+basic workflow components.
 """
 
+from functools import partial
 from typing import Any
 
 import numpy as np
+from numpy.random import default_rng
 from numpy.typing import NDArray
 
 from ropt.config import EnOptConfig
@@ -22,28 +24,43 @@ from ropt.workflow import (
 )
 
 DIM = 5
+UNCERTAINTY = 0.1
 CONFIG: dict[str, Any] = {
     "variables": {
         "variable_count": DIM,
         "perturbation_magnitudes": 1e-6,
     },
+    "realizations": {
+        "weights": [1.0] * 10,
+    },
+    "gradient": {
+        "number_of_perturbations": 5,
+    },
 }
 initial_values = 2 * np.arange(DIM) / DIM + 0.5
 
 
-def rosenbrock(variables: NDArray[np.float64], _: int) -> float:
+def rosenbrock(
+    variables: NDArray[np.float64],
+    realization: int,
+    a: NDArray[np.float64],
+    b: NDArray[np.float64],
+) -> float:
     """Function evaluator for the multi-dimensional rosenbrock function.
 
     Args:
-        variables: The variables to evaluate.
+        variables:    The variables to evaluate.
+        realization:  Realization number.
+        a:            The 'a' parameters.
+        b:            The 'b' parameters.
 
     Returns:
-        The calculated objective
+        The calculated objective.
     """
-    objective: float = 0
+    objective = 0.0
     for d_idx in range(DIM - 1):
         x, y = variables[d_idx : d_idx + 2]
-        objective += (1.0 - x) ** 2 + 100 * (y - x * x) ** 2
+        objective += (a[realization] - x) ** 2 + b[realization] * (y - x * x) ** 2
     return objective
 
 
@@ -68,7 +85,15 @@ def run_optimization(config: dict[str, Any]) -> FunctionResults:
     Returns:
         The optimal results.
     """
-    evaluator = create_evaluator("function_evaluator", functions=[rosenbrock])
+    rng = default_rng(seed=123)
+
+    realizations = len(config["realizations"]["weights"])
+    a = rng.normal(loc=1.0, scale=UNCERTAINTY, size=realizations)
+    b = rng.normal(loc=100.0, scale=100 * UNCERTAINTY, size=realizations)
+
+    evaluator = create_evaluator(
+        "function_evaluator", functions=[partial(rosenbrock, a=a, b=b)]
+    )
     step = create_compute_step("optimizer", evaluator=evaluator)
 
     tracker = create_event_handler("tracker")
@@ -96,8 +121,8 @@ def main() -> None:
     optimal_result = run_optimization(CONFIG)
     assert optimal_result is not None
     assert optimal_result.functions is not None
-    assert np.allclose(optimal_result.functions.weighted_objective, 0, atol=1e-4)
-    assert np.allclose(optimal_result.evaluations.variables, 1, atol=1e-2)
+    assert np.allclose(optimal_result.functions.weighted_objective, 0, atol=1e-1)
+    assert np.allclose(optimal_result.evaluations.variables, 1, atol=1e-1)
 
 
 if __name__ == "__main__":
