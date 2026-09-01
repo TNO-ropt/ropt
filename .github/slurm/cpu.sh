93da905c15dec0320cb7acb9fa0b7c59af9176a3
Peter Verveer <pieter.verveer@tno.nl>
2026-09-01 17:41:08 +0200

    Fix hpc test workflow

--- .github/slurm/cpu.sh
+++ .github/slurm/cpu.sh
@@ -4,6 +4,8 @@
 #SBATCH --output={{output}}
 #SBATCH --chdir={{working_directory}}
 #SBATCH --ntasks={{cores}}
+# Make sure not all memory is claimed:
+#SBATCH --mem-per-cpu=512M
 {%- if run_time_max %}
 #SBATCH --time={{ [1, run_time_max // 60]|max }}
 {%- endif %}
